479a1c07b646d4b4d2c64acd7e0ee44750ab049a
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-08-02 22:09:38 -0400

    Init proyect

---
+++ app/src/main/java/com/example/articlecompose/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.articlecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.articlecompose.ui.theme.ArticleComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArticleComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Presentacion()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Presentacion(modifier: Modifier = Modifier)
+{
+    val image = painterResource(R.drawable.bg_compose_background)
+
+    Column(modifier = modifier.fillMaxSize(),verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Image(
+            painter = image,
+            contentDescription = null,
+        )
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextPreview() {
+    ArticleComposeTheme {
+        Presentacion()
+    }
+}
